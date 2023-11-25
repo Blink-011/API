5d1ecfc0ee7991d2d050b2e866f00a30c954ad6c
sty011 <styse1011@example.com>
2023-11-25 23:48:05 +0330

    Add title and description to Track table

--- Blink/db/migrate/20231124135821_create_tracks.rb
+++ Blink/db/migrate/20231124135821_create_tracks.rb
@@ -1,7 +1,9 @@
 class CreateTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :tracks do |t|
-
+      t.string :title
+      t.text :description
+      
       t.timestamps
     end
   end
