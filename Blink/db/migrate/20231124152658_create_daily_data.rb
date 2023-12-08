5e7bed3b6df37ce1f2d806b66e1c16cae26e7d69
sty011 <styse1011@example.com>
2023-12-08 23:51:42 +0330

    Add user, track, track_item foreign kies to daily_data table

--- Blink/db/migrate/20231124152658_create_daily_data.rb
+++ Blink/db/migrate/20231124152658_create_daily_data.rb
@@ -4,6 +4,9 @@ class CreateDailyData < ActiveRecord::Migration[7.0]
       t.date :day 
       t.time :start_time
       t.time :end_time
+      belongs_to :user, class_name: "user", foreign_key: "user_id"
+      belongs_to :track, class_name: "track", foreign_key: "track_id"
+      belongs_to :track_item, class_name: "track_item", foreign_key: "track_item_id"
       t.timestamps
     end
   end
