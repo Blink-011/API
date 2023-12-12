ffe9ccf2e7716ea6d3657c57c53f0d24e7d3975c
sty011 <styse1011@example.com>
2023-12-12 17:29:29 +0330

    Add user_id and track_id to users_tracks table

--- Blink/db/migrate/20231208202001_create_users_tracks.rb
+++ Blink/db/migrate/20231208202001_create_users_tracks.rb
@@ -1,6 +1,8 @@
 class CreateUsersTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :users_tracks do |t|
+      t.belongs_to :user, class_name: "user", foreign_key: "user_id"
+      t.belongs_to :track, class_name: "track", foreign_key: "track_id"
 
       t.timestamps
     end
